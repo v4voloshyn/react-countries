53a2aa5c584a4c865ea6d3a12c8e739948db467d
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-01-22 14:34:32 +0200

    feat: display Card List on main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,47 @@
-import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { ALL_COUNTRIES } from './config';
+
 import { Controls } from './components/Controls';
 import { Header } from './components/Header';
 import { Main } from './components/Main';
+import { List } from './components/List';
+import { Card } from './components/Card';
+
+import './App.css';
 
 function App() {
+
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios.get(ALL_COUNTRIES)
+      .then(({ data }) => setCountries(data));
+  }, []);
+
   return (
     <>
-    <Header/>
-    <Main>
-      <Controls/>
-    </Main>
+      <Header />
+      <Main>
+        <Controls />
+        <List>
+          {countries.map(c => {
+            const countryInfo = {
+              img: c.flags.png,
+              name: c.name,
+              info: [
+                {title: 'Population', description: c.population.toLocaleString()},
+                {title: 'Region', description: c.region},
+                {title: 'Capital', description: c.capital},
+              ]
+            };
+            
+            return (
+              <Card key={c.name} {...countryInfo}/>
+            )
+          })}
+        </List>
+      </Main>
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,21 @@
 import styled from "styled-components";
 
-const Wrapper = styled.article``;
+const Wrapper = styled.article`
+  border-radius: var(--bradius);
+  background-color: var(--colors-ui-base);
+  box-shadow: var(--shadow);
+  cursor: pointer;
+  overflow: hidden;
+`;
 
-const CardImage = styled.img``;
+const CardImage = styled.img`
+  display: block;
+  width: 100%;
+  height: 150px;
+  object-fit: cover;
+  object-position: center;
+  box-shadow: var(--shadow);
+`;
 
 const CardBody = styled.div``;
 
@@ -15,7 +28,7 @@ const CartListItem = styled.li``;
 export const Card = ({img, name, info = [], onClick}) => {
   return (
      <Wrapper onClick={onClick}>
-       <CardImage/>
+       <CardImage src={img}/>
        <CardBody>
          <CardTitle>{name}</CardTitle>
          <CardList>
