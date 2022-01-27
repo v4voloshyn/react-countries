4ed7afae2eb9ec0865ca6a9d1759a9a537cad2ca
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-01-27 16:32:33 +0200

    feat: done Details and Info components

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,5 +1,41 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { IoArrowBack } from 'react-icons/io5';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Button } from '../components/Button';
+import { Info } from '../components/Info';
+
+import { searchByCountry } from '../config';
 
 export const Details = () => {
+  const { name } = useParams();
+
+  const [country, setCountry] = useState(null);
+  console.log(country);
+
+  useEffect(() => {
+      axios.get(searchByCountry(name)).then(
+        ({data}) => setCountry(data[0])
+      )
+  }, [name]);
+
+
+  const navigate = useNavigate();
+  const goToCountry = (name) => {
+    navigate(`/country/${name}`);
+  }
   
-  return <div>Details {console.log()}</div>;
+  const goBack = () => {
+    navigate(-1);
+  }
+
+
+  return <div>
+    <Button onClick={goBack}>
+      <IoArrowBack/> Back
+    </Button>
+
+    {country && (<Info {...country} param={name} goToCountry={goToCountry}/>)}
+    
+  </div>;
 };
